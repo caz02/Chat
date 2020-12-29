d3a447b30a2332f2f710c70e8afba9d0b1144688
Caroline <76717163+caz02@users.noreply.github.com>
2020-12-29 16:00:06 +0000

    Update chat.php

--- chat.php
+++ chat.php
@@ -9,50 +9,6 @@ if($db->connect_error){
 
 $result = array();
 $message = isset($_POST['message']) ? $_POST['message'] : null;
-$from = isset($_POST['from'] ) ? $_POST['from'] : null;
-
-if(!empty($message) && !empty($from)){
-	$sql = "INSERT INTO 'chat' ('message','from') VALUES ('".$message."','".$from."')";
-	$result['send_status'] = $db->query($sql);
-
-}
-
-$db->close();
-
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-echo json_encode($result);<?php
-
-$db = new mysqli("localhost","root","","chat");
-if($db->connect_error){
-	die("Connection failed: " . $db->connect_error);
-
-}
-
-
-$result = array();
-$message = isset($_POST['message']) ? $_POST['message'] : null;
-$from = isset($_POST['from']) ? $_POST['from'] : null;
-
-if(!empty($message) && !empty($from)){
-	$sql = "INSERT INTO 'chat' ('message','from') VALUES ('".$message."','".$from."')";
-	$result['send_status'] = $db->query($sql);
-
-}
-
-//print messages
-$start = isset($_GET['start']) ? intval($_GET['start']) : 0;<?php
-
-$db = new mysqli("localhost","root","","chat");
-if($db->connect_error){
-	die("Connection failed: " . $db->connect_error);
-
-}
-
-
-$result = array();
-$message = isset($_POST['message']) ? $_POST['message'] : null;
 $from = isset($_POST['from']) ? $_POST['from'] : null;
 
 if(!empty($message) && !empty($from)){
@@ -64,19 +20,7 @@ if(!empty($message) && !empty($from)){
 //print messages
 $start = isset($_GET['start']) ? intval($_GET['start']) : 0;
 $items = $db->query("SELECT * FROM `chat` WHERE `id` > " . $start);
-while($row = $items->fetch_assoc()) {
-	$result['items'][] = $row;
-}
-
-$db->close();
-
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-
-echo json_encode($result);
-$items = $db->query("SELECT * FROM 'chat' WHERE 'id' > " . $start);
-while($row = $items->fetch_assoc()){
-
+while($row = fetch_assoc()) {
 	$result['items'][] = $row;
 }
 
